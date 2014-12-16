4fd598e2aac78ecf7e90e55343805fbcf51862e7
Rituraj <rituraj.tc@gmail.com>
2014-12-16 20:25:55 +0530

    hello

--- test.py
+++ test.py
@@ -33,6 +33,8 @@ dict={}
 req_time = "2014-12-16";
 total_point = 0;
 
+ans_string = req_time;
+
 for sub in reversed(js["result"]):
   
    if( (sub["problem"]["contestId"] , sub["problem"]["index"] ) in dict ):
@@ -64,7 +66,13 @@ for sub in reversed(js["result"]):
 
    total_point = total_point + curr_point;
 
+   ans_string = ans_string + "\n" + str(sub["problem"]["contestId"]) + "   " + str(sub["problem"]["index"]) + "   " + str(curr_point);
+
 print( total_point) ;
+
+ans_string = ans_string + "\n" + " total point is " + str(total_point);
+
+print( ans_string);
 
 # contestId
 # creationTimeSeconds
