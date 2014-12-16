ec738a38469d2220def542b59feaaac5d96cb12b
Rituraj <rituraj.tc@gmail.com>
2014-12-16 06:55:43 +0530

    score working

--- codeforces_score/score/urls.py
+++ codeforces_score/score/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from score import views
 
 urlpatterns = patterns('',
     url(r'^$', views.score, name='score'),
